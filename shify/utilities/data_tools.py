2f8a46482b93de08e8c89dfade6a241b787f105b
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-08-10 11:59:26 +0800

    example string change

--- shify/utilities/data_tools.py
+++ shify/utilities/data_tools.py
@@ -7,7 +7,7 @@ def binance_download():
     parser = argparse.ArgumentParser(description='Data downloader tool for Binance')
     parser.add_argument('-t', '--market_type', help='example : -t futures|spot', required=True)
     parser.add_argument('-i', '--interval'   , help="example : -i 1m|3m|15m|30m|1h...", required=False, default='1m')
-    parser.add_argument('-s', '--symbols'    , help='example : -s BTCUSDT,ETHUSDT,SHIBUSDT', nargs='+', required=True)
+    parser.add_argument('-s', '--symbols'    , help='example : -s BTCUSDT ETHUSDT SHIBUSDT', nargs='+', required=True)
     args = vars(parser.parse_args())
 
     market_type   = args['market_type']
@@ -26,7 +26,7 @@ def mt5_download():
     parser.add_argument('-b', '--broker'  , help="example : -b ava", required=False, default='fx')
     parser.add_argument('-i', '--interval', help="example : -i 1|15|30|60|240|1440...", required=False, default='1')
     parser.add_argument('-y', '--year'    , help="example : -i 2000 ", required=False, default='2000')
-    parser.add_argument('-s', '--symbols' , help='example : -s EURUSD,USDJPY,GBPUSD', nargs='+', required=True)
+    parser.add_argument('-s', '--symbols' , help='example : -s EURUSD USDJPY GBPUSD', nargs='+', required=True)
     args = vars(parser.parse_args())
 
     broker_name = args['broker'  ]
